379298f41d0492f94080bbbd39920c5f9bb5dd61
AWachniak <55257233+AWachniak@users.noreply.github.com>
2019-09-25 10:17:42 -0400

    Update credentials-example.php

--- webapp/credentials-example.php
+++ webapp/credentials-example.php
@@ -1,8 +1,11 @@
 <?php
-
+	//sets the value to this computer
 	$dbserver = "localhost";
+	//set to access this database
 	$db = "IT5236";
+	//uses a specific username
 	$dbusername = "put your user name here";
+	//uses a specific password
 	$dbpassword = "put your password here";
 
 ?>
