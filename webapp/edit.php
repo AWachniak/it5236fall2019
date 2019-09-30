cdfadde0f0ffe63077a6d28e146d1bb1990fc1fa
AWachniak <55257233+AWachniak@users.noreply.github.com>
2019-09-30 14:11:28 -0400

    Update edit.php

--- webapp/edit.php
+++ webapp/edit.php
@@ -1,86 +1,84 @@
+
 <?php
-//sets the configuration option value
-
 ini_set('display_errors', 1);
-//sets the configuration option value
 ini_set('display_startup_errors', 1);
-//reports all errors
 error_reporting(E_ALL);
-
-
-
 // Declare the credentials to the database
 $dbconnecterror = FALSE;
 $dbh = NULL;
-
-//requires the file to be presented once
 require_once 'credentials.php';
-
 try{
-	//establishes the database connection
+
 	$conn_string = "mysql:host=".$dbserver.";dbname=".$db;
-	
-	//uses specific credentials
+
 	$dbh= new PDO($conn_string, $dbusername, $dbpassword);
-	// if there is an error in SQL, PDO will throw exceptions and script will stop running
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//ends unless there is an error
+
 }catch(Exception $e){
-	$dbconnecterror = TRUE;
+	//database issues were encountered
+	http_response_code(504);
+	echo "Databsae timeout";
+	exit();
 }
-//if posting
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-	
-	//sets the listID
-	$listID = $_POST['listID'];
-	
-	//if a given key is provided, sets $complete to 1
-	if (array_key_exists('fin', $_POST)) {
-		$complete = 1;
-	//if not, sets $complete to 0
-	} else {
-		$complete = 0;
+
+if ($_SERVER['REQUEST_METHOD'] == "PUT") {
+	if(array_key_exists('listID',$_GET)){
+		$listID = $_GET['listID'];
+	}else{
+		http_response_code(400);
+		echo "Missing listID";
+		exit(); 
 	}
-	//checks if the POST value is empty, if not sets $finBy to null
-	if (empty($_POST['finBy'])) {
-		$finBy = null;
+	$listID = $_GET['listID'];
+	//decode the json body from the request
+	$task = json_decode(file_get_contents('php://input'), true);
+	if (array_key_exists('completed', $task)) {
+		$complete = $task["completed"];
 	} else {
-		$finBy = $_POST['finBy'];
+		http_response_code(400);
+		echo "Missing completed status";
+		exit();
 	}
-	//sets the listitem
-	$listItem = $_POST['listItem'];
-	
-	
-	//if there is an error
+	if (array_key_exists('taskName', $task)) {
+		$taskName = $task["taskName"];
+	} else {
+		http_response_code(400);
+		echo "Missing taskName";
+		exit();
+	}
+	if (array_key_exists('taskDate', $task)) {
+		$taskDate = $task["taskDate"];
+	} else {
+		http_response_code(400);
+		echo "Missing taskDate";
+		exit();
+	}
 	if (!$dbconnecterror) {
 		try {
-			//updates sql valaues
 			$sql = "UPDATE doList SET complete=:complete, listItem=:listItem, finishDate=:finishDate WHERE listID=:listID";
-			//executes prepared sql statement
-			$stmt = $dbh->prepare($sql);		
-			//binds the value for the sql statement
+			$stmt = $dbh->prepare($sql);
 			$stmt->bindParam(":complete", $complete);
-			//binds the value for the sql statement
-			$stmt->bindParam(":listItem", $listItem);
-			//binds the value for the sql statement
-			$stmt->bindParam(":finishDate", $finBy);
-			//binds the value for the sql statement
+			$stmt->bindParam(":listItem", $taskName);
+			$stmt->bindParam(":finishDate", $taskDate);
 			$stmt->bindParam(":listID", $listID);
+			$response = $stmt->execute();
+			http_response_code(204);
+			
+			exit();
 
-			//executes the sql statements
-			$response = $stmt->execute();	
-			
-			//sends a raw http header
-			header("Location: index.php");
-			
-		//ends unless the header does not send, sends this header
 		} catch (PDOException $e) {
-			header("Location: index.php?error=edit");
-			
+			http_response_code(504);
+			echo "Database exception";
+			exit();
+
 		}
-	//otherwise, sends this header
 	} else {
-		header("Location: index.php?error=edit");
+		http_response_code(504);
 	}
+} else {
+	http_response_code(405);//method not allowed
+	echo "Expected PUT";
+	exit();
+	//put
 }
 ?>
