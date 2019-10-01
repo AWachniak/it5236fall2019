25d4ebe3b0ab1691c75397aa8618302f1552eb90
AWachniak <55257233+AWachniak@users.noreply.github.com>
2019-10-01 18:29:34 -0400

    Update task.php

--- api/task.php
+++ api/task.php
@@ -19,7 +19,7 @@ try{
 	echo "Databsae timeout";
 	exit();
 }
-
+//PUT
 if ($_SERVER['REQUEST_METHOD'] == "PUT") {
 	if(array_key_exists('listID',$_GET)){
 		$listID = $_GET['listID'];
@@ -74,6 +74,7 @@ if ($_SERVER['REQUEST_METHOD'] == "PUT") {
 	} else {
 		http_response_code(504);
 	}
+//DELETE
 } else if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
 	if(array_key_exists('listID',$_GET)){
 		$listID = $_GET['listID'];
@@ -103,10 +104,95 @@ if ($_SERVER['REQUEST_METHOD'] == "PUT") {
 	} else {
 		http_response_code(504);
 	}
-
+//GET
 } else if ($_SERVER['REQUEST_METHOD'] == "GET") {
+	if(array_key_exists('listID',$_GET)){
+		$listID = $_GET['listID'];
+	}else{
+		http_response_code(400);
+		echo "Missing listID";
+		exit(); 
+	}
+	$listID = $_GET['listID'];
+	if (!$dbconnecterror) {
+		
+		try {
+			$sql = "SELECT * FROM doList WHERE listID=:listID";
+			$stmt = $dbh->prepare($sql);
+			$stmt->bindParam(":listID", $listID);
+			$response = $stmt->execute();
+			$result = $stmt->fetch(PDO::FETCH_ASSOC);
+			if (!is_array($result)){
+				http_response_code(404);
+				exit();
+			}
+			
+			http_response_code(200);
+			echo json_encode ($result);
+			exit();
+
+		} catch (PDOException $e) {
+			http_response_code(504);
+			echo "Database exception";
+			exit();
+
+		}
+	} else {
+		http_response_code(504);
+	}
 	
+//POST	
 } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	$task = json_decode(file_get_contents('php://input'), true);
+	if (array_key_exists('completed', $task)) {
+		$complete = $task["completed"];
+	} else {
+		http_response_code(400);
+		echo "Missing completed status";
+		exit();
+	}
+	if (array_key_exists('taskName', $task)) {
+		$taskName = $task["taskName"];
+	} else {
+		http_response_code(400);
+		echo "Missing taskName";
+		exit();
+	}
+	if (array_key_exists('taskDate', $task)) {
+		$taskDate = $task["taskDate"];
+	} else {
+		http_response_code(400);
+		echo "Missing taskDate";
+		exit();
+	}
+	if (!$dbconnecterror) {
+		try {
+			$sql = "INSERT INTO doList (complete, listItem, finishDate) values(:complete,:listItem,:finishDate)";
+			$stmt = $dbh->prepare($sql);
+			$stmt->bindParam(":complete", $complete);
+			$stmt->bindParam(":listItem", $taskName);
+			$stmt->bindParam(":finishDate", $taskDate);
+			$response = $stmt->execute();
+			$taskID = $dbh->lastInsertId();
+				$fulltask = [
+				"listID"=>$taskID,
+				"complete"=>$complete,
+				"listItem"=>$taskName,
+				"finishDate"=>$taskDate
+				];
+			http_response_code(201);
+			echo json_encode($fulltask);
+			exit();
+
+		} catch (PDOException $e) {
+			http_response_code(504);
+			echo "Database exception";
+			exit();
+
+		}
+	} else {
+		http_response_code(504);
+	}
 	
 } else {
 	http_response_code(405);//method not allowed
