238de7426560b97a91b182bbd407528a6943c963
AWachniak <55257233+AWachniak@users.noreply.github.com>
2019-10-02 13:54:36 -0400

    Update edit.php

--- webapp/edit.php
+++ webapp/edit.php
@@ -15,34 +15,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$finBy = $_POST['finBy'];
 	}$listItem = $_POST['listItem'];
 	
-	if (!$dbconnecterror) {
-		try{
-			//call to get api
-			// api url 
-			$url="http://3.230.84.0/api/task.php?listID=$listID";
+	//call to get api
+	// api url 
+	$url="http://3.230.84.0/api/task.php?listID=$listID";
 			
-			$data = array('completed'=>'$complete','taskName'=>'$finBy','taskDate'=>$finBy);
-			$data_json = json_encode($data);
+	$data = array('completed'=>'$complete','taskName'=>'$finBy','taskDate'=>$finBy);
+	$data_json = json_encode($data);
 
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-			curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			//body of response
-			$response = curl_exec($ch); 
-			$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-			$response  = curl_exec($ch);
-			curl_close($ch);
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($data_json)));
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+	curl_setopt($ch, CURLOPT_POSTFIELDS,$data_json);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	//body of response
+	$response = curl_exec($ch); 
+	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+	$response  = curl_exec($ch);
+	curl_close($ch);
 			
-			//staus code 204
-			if($httpcode=='204'){
-				header("Location: index.php");
-			} else {
-				header("Location: index.php?error=edit");
-			}
+	//staus code 204
+	if($httpcode=='204'){
+		header("Location: index.php");
+	} else {
+		header("Location: index.php?error=edit");
+	}
 		
 	
-}
 ?>
