d35287615274b733adacb89d64bd3f09dcceff27
EC2 Default User <ec2-user@ip-172-31-86-250.ec2.internal>
2019-10-22 22:19:49 +0000

    Make tasks talk to the database

--- api/tasks.php
+++ api/tasks.php
@@ -1,27 +1,49 @@
 <?php
+// Declare the credentials to the database
+$dbconnecterror = FALSE;
+$dbh = NULL;
+require_once 'credentials.php';
+try{
+
+	$conn_string = "mysql:host=".$dbserver.";dbname=".$db;
+
+	$dbh= new PDO($conn_string, $dbusername, $dbpassword);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}catch(Exception $e){
+	//database issues were encountered
+	http_response_code(504);
+	echo "Databsae timeout";
+	exit();
+}
+
 //GET
 if($_SERVER['REQUEST_METHOD'] == "GET") {
-	
-	$listID = $_GET['listID'];		
-	try {
-		$sql = "SELECT * FROM doList";
-		$stmt = $dbh->prepare($sql);
-		$response = $stmt->execute();
-		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		if (!is_array($result)){
-			http_response_code(404);
+	if($dbconnecterror){
+			$listID = $_GET['listID'];		
+		try {
+			$sql = "SELECT * FROM doList";
+			$stmt = $dbh->prepare($sql);
+			$response = $stmt->execute();
+			$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			if (!is_array($result)){
+				http_response_code(404);
+				exit();
+			}
+			
+			http_response_code(200);
+			echo json_encode ($result);
+			exit();
+
+		} catch (PDOException $e) {
+			http_response_code(504);
+			echo "Database exception";
 			exit();
 		}
-		
-		http_response_code(200);
-		echo json_encode ($result);
-		exit();
-
-	} catch (PDOException $e) {
+	} else {
 		http_response_code(504);
 		echo "Database exception";
 		exit();
-
 	}
 } else {
 	http_response_code(405);//method not allowed
